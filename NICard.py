29c8f8dcc43cace6878ec04b5cbd2ca0a36e6f73
Marcin Kalinowski <mj.kalinowski1@gmail.com>
2025-06-04 22:22:45 -0400

    card is either analog or digital

--- NICard.py
+++ NICard.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from Sequences import AOSequence, DOSequence
+from Sequences import Sequence, DOSequence, AOSequence
 import numpy as np
 import os
 
@@ -12,153 +12,125 @@ class NICard:
     def __init__(
         self,
         device_name: str,  # e.g., "PXI1Slot3"
-        sample_rate_ao: int,
-        sample_rate_do: Optional[int] = None,
-        sequence_ao: Optional[List[AOSequence]] = None,
-        sequence_do: Optional[List[DOSequence]] = None,
+        sample_rate: int,
+        sequences: Optional[List[Sequence]] = None,
         trigger_source: str = None,
-        is_master: bool = False
+        clock_source: str = None,
+        is_primary: bool = False
     ):
         """
         Initialize a NI card with its metadata and sequences.
 
         Args:
             device_name (str): Name of the NI device (e.g., "PXI1Slot3")
-            sample_rate_ao (int): Sample rate in Hz for analog output
-            sample_rate_do (int, optional): Sample rate in Hz for digital output. If None, will use the sample rate of the analog output.
-            sequence_ao (List[AOSequence], optional): List of analog output sequences
-            sequence_do (List[DOSequence], optional): List of digital output sequences
+            sample_rate (int): Sample rate in Hz for all sequences
+            sequences (List[Sequence], optional): List of sequences
+            is_digital (bool, optional): Whether the card is digital. If True, will be used to determine the sample rate for the digital output.
             trigger_source (str)
-            is_master (bool, optional): Whether the card is the master card. If True, will be used to determine the sample rate for the digital output.
+            is_primary (bool, optional): Whether the card is the primary card. If True, will be used to determine the sample rate for the digital output.
         """
         self.device_name = device_name
-        self.sequence_ao = sequence_ao or []
-        self.sequence_do = sequence_do or []
+        self.sequences = sequences or []
 
         # Save the sample rates for analog and digital output
-        self.sample_rate_ao = int(sample_rate_ao)
-        
-        # Set sample rate for digital output if provided, otherwise use the sample rate of the analog output
-        if sample_rate_do:
-            self.sample_rate_do = int(sample_rate_do)
-        else:
-            self.sample_rate_do = self.sample_rate_ao
+        self.sample_rate = int(sample_rate)
 
-        self.is_master = is_master
+        # Whether the card is the primary card
+        self.is_primary = is_primary
 
-        # Placeholder for the chunk sizes
-        self.chunk_size_ao = None
-        self.chunk_size_do = None
+        # Placeholder for channel IDs
+        self.channel_ids = set()
 
-        # Placeholder for the trigger source
+        # Placeholder for the trigger source and clock source
         self.trigger_source = trigger_source
+        self.clock_source = clock_source
 
         # Create shared memory segment names for this card
-        self.shm_name_ao = f"nistreamer_shared_memory_{os.getpid()}_{self.device_name}_ao"
-        self.shm_name_do = f"nistreamer_shared_memory_{os.getpid()}_{self.device_name}_do"
+        self.shm_name = f"nishm_{os.getpid()}_{self.device_name}"
 
         # Validate the sequences
-        self._validate_ao_sequences()
-        self._validate_do_sequences()
+        self._validate_sequences()
 
-        # Setup trigger source
-        self._setup_trigger()
-
-    def _setup_trigger(self):
-        """If master, create the string for the trigger source. If not, just save."""
-        if self.is_master:
-            if not self.trigger_source:
-                raise ValueError(f"Trigger source is not set for master card")
-            self.trigger_source = f"{self.device_name}/{self.trigger_source}"
-        else:
-            self.trigger_source = self.trigger_source
-
-
-    def _validate_ao_sequences(self):
-        # Make sure all the channel IDs from the analog sequences are unique and print the repeated ones.
-        self.channel_ids_ao = set([seq.channel_id for seq in self.sequence_ao])
-        if len(self.channel_ids_ao) != len(self.sequence_ao):
-            raise ValueError(f"Reused analog channels found")
+    def _validate_sequences(self):
+        # Make sure all the channel IDs from the sequences are unique and print the repeated ones.
+        self.channel_ids, counts = np.unique([seq.channel_id for seq in self.sequences], return_counts=True)
+        if len(self.channel_ids) != len(self.sequences):
+            raise ValueError(f"Reused channels found: {list(self.channel_ids[counts > 1])}")
         
         # Make sure all the sequences have the same sample rate and that they match the card sample rate
-        if not np.all(np.array([seq.sample_rate for seq in self.sequence_ao]) == self.sample_rate_ao):
-            raise ValueError(f"Analog sequences do not match the card sample rate")
-
-    def _validate_do_sequences(self):
-        # Make sure all the channel IDs from the digital sequences are unique and print the repeated ones.
-        self.channel_ids_do = set([seq.channel_id for seq in self.sequence_do])
-        if len(self.channel_ids_do) != len(self.sequence_do):
-            raise ValueError(f"Reused digital channels found")
+        if not np.all(np.array([seq.sample_rate for seq in self.sequences]) == self.sample_rate):
+            raise ValueError(f"Sequences do not match the card sample rate")
         
-        # Make sure all the sequences have the same sample rate and that they match the card sample rate
-        if not np.all(np.array([seq.sample_rate for seq in self.sequence_do]) == self.sample_rate_do):
-            raise ValueError(f"Digital sequences do not match the card sample rate")
+        # If there are sequences, infer whether the card is analog or digital
+        if len(self.sequences):
+            self.is_digital = isinstance(self.sequences[0], DOSequence)
 
-    def add_ao_sequence(self, sequence: AOSequence):
+            # Ensure all sequences are of the same type
+            mode_agreement = [isinstance(seq, DOSequence if self.is_digital else AOSequence) for seq in self.sequences]
+            if not np.all(mode_agreement):
+                raise ValueError(f"Card is {'digital' if self.is_digital else 'analog'} but some sequences are not ({[self.sequences[i] for i in np.where(~np.array(mode_agreement))[0]]})")
+
+    def add_sequence(self, sequence: Sequence):
         """Add an analog output sequence to the card."""
         # Make sure the sequence has the same sample rate as the card
-        if sequence.sample_rate != self.sample_rate_ao:
-            raise ValueError(f"Sequence sample rate {sequence.sample_rate} does not match card sample rate {self.sample_rate_ao}")
+        if sequence.sample_rate != self.sample_rate:
+            raise ValueError(f"Sequence sample rate {sequence.sample_rate} does not match card sample rate {self.sample_rate}")
         
         # Check for duplicate channel IDs
-        if sequence.channel_id in self.channel_ids_ao:
+        if sequence.channel_id in self.channel_ids:
             raise ValueError(f"Channel ID {sequence.channel_id} already exists")
         
-        self.sequence_ao.append(sequence)
-
-    def add_do_sequence(self, sequence: DOSequence):
-        """Add a digital output sequence to the card."""
-        # Make sure the sequence has the same sample rate as the card
-        if sequence.sample_rate != self.sample_rate_do:
-            raise ValueError(f"Sequence sample rate {sequence.sample_rate} does not match card sample rate {self.sample_rate_do}")
+        # Make sure the sequence is of the same type as the card
+        if isinstance(sequence, DOSequence) != self.is_digital:
+            raise ValueError(f"Sequence is {'digital' if isinstance(sequence, DOSequence) else 'analog'} but card is {'digital' if self.is_digital else 'analog'}")
         
-        # Check for duplicate channel IDs
-        if sequence.channel_id in self.channel_ids_do:
-            raise ValueError(f"Channel ID {sequence.channel_id} already exists")
-        
-        self.sequence_do.append(sequence)
+        self.sequences.append(sequence)
+        self.channel_ids.add(sequence.channel_id)
 
-    def compile(self, chunk_size_ao: int, chunk_size_do: int):
+    def compile(self, chunk_size: int):
         """
         Compile all sequences with the given chunk size.
 
         Args:
-            chunk_size_ao (int): The chunk size for analog output
-            chunk_size_do (int): The chunk size for digital output
+            chunk_size (int): The chunk size for all sequences
         """
         # Make sure the chunk sizes are integers
-        if not isinstance(chunk_size_ao, int):
-            raise ValueError(f"Chunk size for analog output must be an integer, got {chunk_size_ao}")
-        if not isinstance(chunk_size_do, int):
-            raise ValueError(f"Chunk size for digital output must be an integer, got {chunk_size_do}")
+        if not isinstance(chunk_size, int):
+            raise ValueError(f"Chunk size must be an integer, got {chunk_size}")
 
         # Compile the sequences
-        if self.sequence_ao:
+        if self.sequences:
              # Calculate the max stop sample for analog output
-            max_stop_sample_ao = max([max([ins.end_sample for ins in seq.instructions]) for seq in self.sequence_ao])
-            num_chunks_ao = int(np.ceil(max_stop_sample_ao / chunk_size_ao))
-            for seq in self.sequence_ao:
-                seq.compile(chunk_size_ao, num_chunks_ao)
-        if self.sequence_do:
-            # Calculate the max stop sample for digital output
-            max_stop_sample_do = max([max([ins.end_sample for ins in seq.instructions]) for seq in self.sequence_do])
-            num_chunks_do = int(np.ceil(max_stop_sample_do / chunk_size_do))
-            for seq in self.sequence_do:
-                seq.compile(chunk_size_do, num_chunks_do)
+            max_stop_sample = max([max([ins.end_sample for ins in seq.instructions]) for seq in self.sequences])
+            num_chunks = int(np.ceil(max_stop_sample / chunk_size))
+            self.num_chunks = num_chunks
+            for seq in self.sequences:
+                seq.compile(chunk_size, num_chunks)
+        else:
+            self.num_chunks = 0
 
         # Save chunk sizes to mark that the sequences are compiled
-        self.chunk_size_ao = chunk_size_ao
-        self.chunk_size_do = chunk_size_do
+        self.chunk_size = chunk_size
+
+    def num_channels(self):
+        return len(self.channel_ids)
 
     def get_sequences(self):
+        """Returns the pointer to the sequences array. Do not modify the array!"""
         # Make sure the sequences are compiled
-        if not self.chunk_size_ao or not self.chunk_size_do:
+        if not self.chunk_size:
             raise ValueError(f"Sequences are not compiled. Run NICard.compile() first.")
         
-        return self.sequence_ao, self.sequence_do
+        return self.sequences
     
     def get_trigger_source(self):
-        if self.is_master:
+        if self.is_primary:
             return self.trigger_source
+        else:
+            return None
+    
+    def get_clock_source(self):
+        if self.is_primary:
+            return self.clock_source
         else:
             return None
